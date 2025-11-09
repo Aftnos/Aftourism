07afafb5d9724326e3e6cdddd0321339a478fa72
Aftnos <alyfk@qq.com>
2025-11-09 17:32:41 +0800

    第一次项目创建

---
+++ src/main/java/aftnos/aftourism/AftourismApplication.java
@@ -0,0 +1,13 @@
+package aftnos.aftourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftourismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AftourismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aftnos/aftourism/AftourismApplicationTests.java
@@ -0,0 +1,13 @@
+package aftnos.aftourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftourismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
